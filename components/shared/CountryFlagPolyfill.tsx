17c1a1954670c939f5220a2b47833cd5bec69cb3
Pavel Yampolsky <2pavelya@gmail.com>
2026-01-15 10:13:09 +0100

    Fix flags presentation on Windows OS II

--- components/shared/CountryFlagPolyfill.tsx
+++ components/shared/CountryFlagPolyfill.tsx
@@ -9,7 +9,7 @@
  * The polyfill loads a small Twemoji font (~77KB) only when needed,
  * enabling proper flag display for country selection and location displays.
  *
- * @see https://github.com/nicolo-ribaudo/country-flag-emoji-polyfill
+ * @see https://github.com/talkjs/country-flag-emoji-polyfill
  */
 
 import { useEffect } from 'react'
@@ -17,9 +17,13 @@ import { polyfillCountryFlagEmojis } from 'country-flag-emoji-polyfill'
 
 export function CountryFlagPolyfill() {
   useEffect(() => {
+    // Use local font path instead of CDN for reliable loading
+    // Font file is at: public/fonts/TwemojiCountryFlags.woff2
+    const fontUrl = '/fonts/TwemojiCountryFlags.woff2'
+
     // Only runs on client-side, detects if polyfill is needed
     // Returns true if polyfill was applied, false otherwise
-    polyfillCountryFlagEmojis()
+    polyfillCountryFlagEmojis('Twemoji Country Flags', fontUrl)
   }, [])
 
   // This component doesn't render anything
